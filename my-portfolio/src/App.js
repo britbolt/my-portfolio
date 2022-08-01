6ba5936e28fdf68a4d5a65c3b371e03a345c0e88
britbolt <blattisaw@gmail.com>
2022-08-01 17:39:34 -0400

    added components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
-import 
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+
+
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    
+    <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
 export default App;
 
-mongo "mongodb+srv://clusterbolt.ingk23h.mongodb.net/budget_tracker" --apiVersion 1 --username britbolt
-G5qqy%21ep698c32k
---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Projects(){
+    return (
+
+<section>
+<Container className="home-section" id="home">
+    <Container className="home-content">
+        <Row>
+            <Col md={7} className="home-header">
+                <h1 className="heading">
+                    Welcome!</h1>
+                    </Col>
+                     </Row> 
+    </Container>
+</Container>
+</section>
+);
+    }
+
+    export default Projects;
