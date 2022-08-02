b966dac0fd1d3fdd917fd5fc54fa993843f1b8e9
britbolt <blattisaw@gmail.com>
2022-08-02 13:20:55 -0400

    fixed repo src

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {  Nav, Navbar, Container } from "react-bootstrap";
+
+function NavBar() {
+    return (
+<Navbar bg="light" expand="md">
+    <Container>
+        <Navbar.Brand href="/home">Bb.</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+<Nav className="just">
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/about">About</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/projects">Projects</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/resume">Resume</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    </Navbar.Collapse>
+    </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar;
