6870a6fbd20c62c9bdf1f1654480f379e5aa5bcf
britbolt <blattisaw@gmail.com>
2022-08-03 14:36:46 -0400

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,31 +10,25 @@ function NavBar(props) {
 
     return (
     <Navbar fixed="top" >
-  
         <Navbar.Brand href="home">Bb.</Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          
-<Nav>
-      
-        {categories.map((category) => (
-          
-          <Nav.Link className={`mx-1 ${
-            currentCategory.name === category.name && 'navActive'
-          }`}
-          key={category.name}>
-            <span onClick={() => {
-              setCurrentCategory(category); 
-            }}>
-            </span>
-          </Nav.Link>
-        ))}
-
-    </Nav>
-    
+        <Navbar.Collapse className="justify-content-end"> 
+        <Nav>   
+          {categories.map((category) => (
+        <Nav.Item onClick={() => {
+          setCurrentCategory(category); 
+        }} >
+        <Nav.Link className={`mx-1 ${
+          currentCategory.name === category.name && 'navActive'
+        }`}
+        key={category.name} value={category.name}>
+        </Nav.Link>
+        </Nav.Item>
+      ))}
+        </Nav>  
     </Navbar.Collapse>
    
     </Navbar>
     )
-}
+};
 
 export default NavBar;
